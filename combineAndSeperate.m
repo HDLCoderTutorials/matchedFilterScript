86a7ea943f0a4ae0da9e8981aed7bce54b44feab
Ryder Swan <ryderswan01@gmail.com>
2019-07-22 16:31:09 -0500

    made her look perdy for my presentation

--- combineAndSeperate.m
+++ combineAndSeperate.m
@@ -1,6 +1,6 @@
 %% Variables 
-rxSignalLength = 1000;
-pNCodeLength = 100;
+rxSignalLength = 10000;
+pNCodeLength = 1000;
 pNCodeA = generatePNCode(pNCodeLength,1);
 pNCodeB = generatePNCode(pNCodeLength,6442);
 %pNCodeA = ones(1,pNCodeLength/2);
@@ -9,12 +9,13 @@ pNCodeC = generatePNCode(pNCodeLength,2);
 %% Create an input signal
 rxSignal = zeros(1,rxSignalLength);
 
-rxSignal = addDelayedSignal(rxSignal,pNCodeA,[400,450,500]);
-rxSignal = addDelayedSignal(rxSignal,pNCodeB,[800,450,59]);
-rxSignal = addDelayedSignal(rxSignal,pNCodeC,200);
+rxSignal = addDelayedSignal(rxSignal,pNCodeA,[2000,5000,8000]);
+rxSignal = addDelayedSignal(rxSignal,pNCodeB,[3000,5500,9000]);
+%rxSignal = addDelaydSignal(rxSignal,pNCodeC,200);
 
 %% Create some noise
-noiseAmp = .5;
+rng('shuffle');
+noiseAmp = 2;
 noise = (randn(1,rxSignalLength)-0.5)*2*noiseAmp;
 
 %% Combine the input singal and the noise
@@ -23,23 +24,23 @@ rxSignal = rxSignal + noise;
 %% Step over and plot the auto Corilation
 figure(2);
 
-subplot(4,1,2);
+subplot(3,1,2);
 corOutputA = xcorr(rxSignal,pNCodeA);
 plot(corOutputA(rxSignalLength:end));
 title('Code A Corilation Output');
 
-subplot(4,1,1);
+subplot(3,1,1);
 plot(rxSignal);
 title('raw rxSignal');
 
 
-subplot(4,1,3);
+subplot(3,1,3);
 corOutputB = xcorr(rxSignal,pNCodeB);
 plot(corOutputB(rxSignalLength:end));
 title('Code B Corilation Output');
 
-
-subplot(4,1,4);
-corOutputC = xcorr(rxSignal,pNCodeC);
-plot(corOutputC(rxSignalLength:end));
-title('Code C Corilation Output');
+% 
+% subplot(4,1,4);
+% corOutputC = xcorr(rxSignal,pNCodeC);
+% plot(corOutputC(rxSignalLength:end));
+% title('Code C Corilation Output');
